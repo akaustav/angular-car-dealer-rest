5fb6c6cca554af8958d6c1ee253148074448efcc
akaustav <akaustav@gmail.com>
2018-06-21 22:47:30 -0700

    - bodyparser

--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
 'use strict';
 const express     = require('express');
-const bodyParser  = require('body-parser');
 const fs          = require('fs');
 const app         = express();
 const customers   = JSON.parse(fs.readFileSync('mock/customers.json', 'utf-8'));
 const cars        = JSON.parse(fs.readFileSync('mock/cars.json', 'utf-8'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 app.get('/api', (req, res) => {
   res.sendFile(__dirname + '/index.html');
